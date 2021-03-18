797b177ab3c49007f22d3008aa9ff126634813d8
taylorsieling <taylorasieling@gmail.com>
2021-03-18 14:00:59 -0500

    commit tree 61f8f8a34fcc9d4e57b99a83707225d3b7708af4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(param="special") {
+        return `You are ${flair}${param}${flair}!`
+    };
+}
+
+const Calculator = {
+    add: function() {return (1+3)},
+    subtract: function() {return (1-3)},
+    multiply: function() {return (1*3)},
+    divide: function() {return (10/5)}
+}
+
+function actionApplyer(integer, array) {
+    if (array === []) {
+        return integer
+    } else {
+        return array.reduce(function(int, func) { return func(int)
+        }, integer);
+    };
+}
